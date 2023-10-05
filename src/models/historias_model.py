5901972a9a0208203f0e82b568f81ac9dabfd89b
RaulRinconX <r.rincon@uniandes.edu.co>
2023-10-05 01:11:45 -0500

    cambio de query

--- src/models/historias_model.py
+++ src/models/historias_model.py
@@ -10,13 +10,15 @@ class historiaModel():
             historias=[]
 
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM HISTORIAS ORDER BY ID")
+                cursor.execute("SELECT * FROM HISTORIAS")
                 resultset=cursor.fetchall()
 
                 for row in resultset:
                     historia=Historias(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
                     historias.append(historia.to_JSON())
+                    
             connection.close()
             return historias
+        
         except Exception as e:
             raise Exception(e)
