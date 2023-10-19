815c34050f30aaec5cfdb532649c1b7c311081c7
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2023-10-18 22:11:42 -0500

    correcion

--- src/routes/citas.py
+++ src/routes/citas.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify
 from models.citas_model import citasModel
 
-main=Blueprint('historia_blueprint',__name__)
+main=Blueprint('citas_blueprint',__name__)
 
 @main.route('/')
-def get_historias():
+def get_citas():
     try:
         citas = citasModel.get_citas()
         return jsonify(citas)
