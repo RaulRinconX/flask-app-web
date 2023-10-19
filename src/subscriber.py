601ee17a5c20ed90fc39ddf5a7c01d8c0eb90662
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2023-10-18 23:27:17 -0500

    yo confio

--- src/subscriber.py
+++ src/subscriber.py
@@ -1,8 +1,10 @@
-import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import pika
+import json
 
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 rabbit_host = '10.128.0.13'
 rabbit_user = 'monitoring_user'
@@ -23,20 +25,24 @@ for topic in topics:
     channel.queue_bind(
         exchange=exchange, queue=queue_name, routing_key=topic)
 
-print('> Waiting measurements. To exit, press CTRL+C')
+print('> Waiting citas. To exit, press CTRL+C')
 
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected")
 
 def callback(ch, method, properties, body):
-    payload = json.loads(body.decode('utf8').replace("'", '"'))
-    topic = method.routing_key.split('.')
-    emit('measurement', {
-        'variable_name': topic[2],
-        'value': payload['value'],
-        'unit': payload['unit'],
-        'topic': topic[0] + topic[1]
-    })
+    try:
+        payload = json.loads(body)
+        topic = method.routing_key.split('.')
+        emit('cita', payload)  # Emitir el payload JSON a los clientes conectados
+    except json.JSONDecodeError:
+        print("Error al decodificar el mensaje JSON:", body)
 
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 channel.start_consuming()
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
