7e18ce95e970c9e91f9e742c77dc4a27715b60a5
HsanAnto <98660754+HsanAnto@users.noreply.github.com>
2023-10-20 12:01:44 -0500

    epic momento

--- src/producer.py
+++ src/producer.py
@@ -11,6 +11,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aykutrincon@gmail.com'
 EMAIL_HOST_PASSWORD = 'scfuiloumslnoaah'
 
+
+correos_enviados = set()
+
 print('> Sending citas. To exit press CTRL+C')
 
 url = 'http://34.160.204.45:80/api/citas-actual'
@@ -22,14 +25,19 @@ while True:
     current_time = datetime.now()
 
     for cita in data:
+        id_cita = cita['id']
         fecha_cita = datetime.strptime(cita['fecha'], '%d/%m/%Y %H:%M:%S')
         two_hours_later = current_time + timedelta(hours=2)
         recipient_email = cita['correo_paciente']
+        sent_email = (id_cita, recipient_email)
+
+        if sent_email in correos_enviados:
+            continue
 
         if fecha_cita <= two_hours_later:
             # Envía un correo a la persona
             subject = 'Recordatorio de cita'
-            message = f'Esto es un recordatorio de su cita: {cita}'
+            message = f"Estimado(a) paciente,\n\nEste es un recordatorio amistoso de su cita en RasiMedical programada para {fecha_cita}.\n\nPor favor, asegúrese de llegar a tiempo. Si tiene alguna pregunta o necesita reprogramar su cita, no dude en contactarnos.\n\nRasiMedical"
             from_email = 'aykutrincon@gmail.com'
 
             msg = MIMEMultipart()
@@ -45,6 +53,9 @@ while True:
             server.sendmail(from_email, recipient_email, text)
             server.quit()
 
-            print(f"Correo enviado para la cita: {cita}")
+            correos_enviados.add(sent_email)
 
-    time.sleep(5)
+            print(f"Correo enviado para la cita: {cita}")
+            
+
+    time.sleep(5)   
