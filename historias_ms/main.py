48e548bc1aa03421890c3e2b8b205996d89af0f3
Abel Arismendy <abel@arismendy.co>
2023-12-05 18:29:11 -0500

    printealo

--- historias_ms/main.py
+++ historias_ms/main.py
@@ -9,6 +9,8 @@ app = FastAPI()
 MONGODB_URL = os.getenv("MONGODB_URL")
 DB_NAME = os.getenv("DB_NAME")
 
+print(MONGODB_URL)
+print(DB_NAME)
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client[DB_NAME]
 
